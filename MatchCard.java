5b902634ca67c671cbbb7238d8d7735c6d3869da
Kitamin199 <aditokta505@gmail.com>
2026-02-20 06:31:13 +0700

    Implement Card class and deck building in MatchCard
    Refactor MatchCard class to include Card inner class and deck initialization.

--- MatchCard.java
+++ MatchCard.java
@@ -1,9 +1,63 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Arraylist;
-import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+import java.util.ArrayList;
+//import java.util.Random;
+//import javax.swing.*;
 
-  public class MatchCard {
+public class MatchCard {
+    // sun.font.FontDesignMetrics a;
+    // public static void main(String[] args) {
+    //     int a = 2;
+    //     int hitung = a * 2;                        
 
-  }
-  
+    //     System.out.print(hitung);
+    //     System.out.println("Jambi Butuk");
+    //     int l = 7;
+    //     boolean a = true;
+    //     char m = a ? 'T' : 'F';
+    //     System.out.println(m);
+
+    private class Card {
+        String value;
+        String type;
+
+        Card(String value, String type) {
+            this.value = value;
+            this.type = type;
+        }
+
+        public String toString() {
+            return value + " of " + type;
+        }
+    }
+
+    ArrayList<Card> deck;
+
+    MatchCard () {
+        startGame();
+    }
+
+    public void startGame() {
+        buildDeck();
+    }
+
+    public void buildDeck() {
+        deck = new ArrayList<Card>();
+        String[] types = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        for (int i = 0; i < types.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+               Card card = new Card(values[j], types[i]);
+                deck.add(card);
+            }
+        }
+        System.out.println("Deck built with ");
+        System.out.println(deck);
+    }
+
+
+            
+}
+
+
