43ea281bba60cbac7837d5a15f314758eb53d5cb
Alfinandra Rashyid Pratama <lilkyogre03@gmail.com>
2026-03-05 12:52:39 +0000

    alfi 3

---
+++ gambartarot.java
@@ -0,0 +1,85 @@
+public class gambartarot extends tarot {
+
+    public gambartarot(String namakartu){
+        super(namakartu);
+      
+    }
+
+    @Override
+    public void showkartu() {
+        super.showkartu();
+         switch (namakartu) {
+        case "THE FOOL":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE MAGICIAN":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE HIGH PRIESTES":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+       case "THE EMPRESS":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE EMPEROR":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+         case "THE HIEROPHANT":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE LOVERS":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE CHARIOT":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+       case "THE STRENGHT":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE HERMIT":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+         case "WHEEL OF FORTUNE":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "JUSTICE":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE HANGED MAN":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+       case "DEATH":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "TEMPERANCE":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+         case "THE DEVIL":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE TOWER":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE STAR":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+       case "THE MOON":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE SUN":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "JUDGEMENT":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        case "THE WORLD":
+            System.out.println(" ||| gambar kartu"+" "+namakartu+"|||");
+            break;
+        
+       
+        default:
+            break;
+       }
+    }
+
+}
